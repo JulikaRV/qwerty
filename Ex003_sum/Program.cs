695adf87afe308960b7cc88f1a92c17679aa7bf0
Julia <julia.rogovaja@gmail.com>
2022-06-28 19:52:00 +0300

    sum

---
+++ Ex003_sum/Program.cs
@@ -0,0 +1,14 @@
+﻿int numberA = 3;
+int numberB = 8;
+Console.WriteLine(numberA + numberB);
+
+int numberC = 3;
+int numberD = 88;
+int result = numberC + numberD;
+Console.WriteLine(result);
+
+// Console.WriteLine("input X");
+// int X = Console.ReadLine();
+// Console.WriteLine("input Y");
+// int Y = Console.ReadLine();
+// Console.WriteLine(X / Y);
